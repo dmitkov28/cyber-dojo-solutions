2ca15576382db2252fca7d22b119476f0b46da44
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-01-04 16:06:24 +0200

    green: implement combinations checking function

--- harry_potter/solution.py
+++ harry_potter/solution.py
@@ -1,3 +1,7 @@
+import itertools
+from typing import List
+
+
 def calculate_discount(n_books: int) -> float:
     SINGLE_BOOK_PRICE = 8
     SINGLE_BOOK_DISCOUNT_RATE = 0
@@ -17,8 +21,10 @@ def calculate_discount(n_books: int) -> float:
 
     if n_books == 4:
         return n_books * (1 + FOUR_BOOKS_DISCOUNT_RATE)
-    
+
     if n_books == 5:
         return n_books * (1 + FIVE_BOOKS_DISCOUNT_RATE)
-    
-    
+
+
+def has_possible_combinations(input_data: List[int], n_combinations: int) -> bool:
+    return len(set(itertools.combinations(input_data, n_combinations))) > 0
